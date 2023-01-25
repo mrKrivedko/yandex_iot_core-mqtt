8c206785c1fdad2e8898af9d14719b163f0f695f
mrKrivedko <mr.krivedko.ds@gmail.com>
2023-01-25 15:03:01 +0600

    modify settings.py

--- pub.py
+++ pub.py
@@ -1,23 +1,24 @@
 from paho.mqtt.client import MQTT_ERR_SUCCESS
+import os
 import sys
 
 import settings
-from client import YaClient
+from ya_client import YaClient
 
 
 def main():
-    dev = YaClient('Test_Device_Client')
+    dev = YaClient(settings.DEVICE_NAME)
     reg = YaClient(settings.REGISTRY_NAME)
 
     if settings.USE_DEVICE_LOGIN_PASSWORD:
         dev.start_with_login(settings.DEVICE_ID, settings.DEVICE_PASSWORD)
     else:
-        dev.start_with_cert('mptk_register/device1/cert_dev.pem', 'mptk_register/device1/key_dev.pem')
+        dev.start_with_cert(settings.DEVICE_CERT, settings.DEVICE_KEY)
 
-    if settings.USE_REGISTRY_LOGIN_PASSWORD:
-        reg.start_with_login(settings.REGISTRY_ID, settings.REGISTRY_PASSWORD)
-    else:
-        reg.start_with_cert('mptk_register/cert_reg.pem', 'mptk_register/key_reg.pem')
+    # if settings.USE_REGISTRY_LOGIN_PASSWORD:
+    #     reg.start_with_login(settings.REGISTRY_ID, settings.REGISTRY_PASSWORD)
+    # else:
+    #     reg.start_with_cert('mptk_register/cert_reg.pem', 'mptk_register/key_reg.pem')
 
     # res, _ = dev.subscribe(settings.REGISTRY_COMMANDS)
     # if res != mqtt.MQTT_ERR_SUCCESS:
@@ -30,8 +31,9 @@ def main():
     # res = reg.publish(settings.REGISTRY_COMMANDS, "reg commands")
     # if res != MQTT_ERR_SUCCESS:
     #     sys.exit("Can't publish [ " + settings.REGISTRY_COMMANDS + " ]")
+    payload = input()
 
-    res = dev.publish(settings.REGISTRY_EVENTS, "reg events!!!")
+    res = dev.publish(settings.REGISTRY_EVENTS, payload)
     if res != MQTT_ERR_SUCCESS:
         sys.exit("Can't publish [ " + settings.REGISTRY_EVENTS + " ]")
 
--- ya_client.py
+++ ya_client.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-import sys
 import threading
 from enum import IntEnum
 
@@ -7,13 +6,16 @@ import settings
 
 
 class Qos(IntEnum):
+    """Enum where members are also (and must be) ints"""
+
     AT_MOST_ONCE = 0
     AT_LEAST_ONCE = 1
 
 
 class YaClient:
+    """Реализация клиента для Yandex IoT Core."""
 
-    def __init__(self, client_id):
+    def __init__(self, client_id: str) -> None:
         self.received = threading.Event()
         self.qos = Qos.AT_LEAST_ONCE
         self.client = mqtt.Client(client_id)
@@ -25,7 +27,7 @@ class YaClient:
         self.client.connect(settings.MQTT_SERVER, settings.MQTT_PORT, 60)
         self.client.loop_start()
 
-    def start_with_login(self, login, password):
+    def start_with_login(self, login: str, password: str):
         self.client.tls_set(settings.ROOTCA_PATH)
         self.client.username_pw_set(login, password)
         self.client.connect(settings.MQTT_SERVER, settings.MQTT_PORT, 60)
@@ -34,21 +36,34 @@ class YaClient:
     def disconnect(self):
         self.client.loop_stop()
 
-
     @staticmethod
-    def on_message(client, userdata, message):
+    def on_message(
+        client: mqtt.Client,
+        userdata: threading.Event,
+        message: mqtt.MQTTMessage
+    ):
+        """
+        Печатает в консоль информацию о сообщении.
+        Устанавливает Event.set().
+        """
         print("Received message '" + str(message.payload) + "' on topic '"
               + message.topic + "' with QoS " + str(message.qos))
         userdata.set()
 
-    def publish(self, topic, payload):
-        rc = self.client.publish(topic, payload, self.qos)
+    def publish(self, topic: str, payload: str) -> int:
+        """Публикуем."""
+        rc: mqtt.MQTTMessageInfo = self.client.publish(topic, payload, self.qos)
         rc.wait_for_publish()
         return rc.rc
 
     def subscribe(self, topic):
+        """Подписываемся через paho.mqtt.client."""
         return self.client.subscribe(topic, self.qos)
 
     def wait_subscribed_data(self):
+        """
+        После вызова userdata.set(), вызывает ожидание Event.wait()
+        и после clear() снова будет ждать userdata.set().
+        """
         self.received.wait()
         self.received.clear()
